c4a9a44329646f6a6ff6e3eefa3ae4c94da7ca21
Tesloboy <meex_v92@mail.ru>
2023-05-12 19:45:39 +0800

    Initial Commit

---
+++ ContactList 3.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList 3.0
+//
+//  Created by Viktor Teslenko on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
